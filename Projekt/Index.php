e2e815ca4b07665ace808d99404c3b70359de664
Fabian Kurt <pumba99@outlook.com>
2018-03-13 11:14:55 +0100

    Projekt Start

---
+++ Projekt/Index.php
@@ -0,0 +1,14 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Modul-152</title>
+    <link rel="stylesheet" href="#" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+  <body>
+
+  </body>
+</html>
